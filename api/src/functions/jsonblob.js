04daa0abc87c71da29ce40fa8111bed163929005
David A <danderson@netbizsystems.com>
2025-08-06 08:48:37 -0400

    wip

--- api/src/functions/jsonblob.js
+++ api/src/functions/jsonblob.js
@@ -1,6 +1,6 @@
 
 
-const { app } = require('@azure/functions');
+const { app } = require('@azure/functions');  
 const { BlobServiceClient } = require('@azure/storage-blob');
 const { DefaultAzureCredential } = require("@azure/identity");
 
